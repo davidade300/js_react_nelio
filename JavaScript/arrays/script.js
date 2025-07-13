341ea8afc108a9819e9110fc21ada86bc5272276
davidade300 <iaderinho@icloud.com>
2025-07-13 20:34:23 -0300

    codigos basicos sobre array adicionados

--- JavaScript/arrays/script.js
+++ JavaScript/arrays/script.js
@@ -26,7 +26,42 @@ alturas.unshift(1.58); // adiciona como primeiro elemento do array
 console.log("\nTESTE push E unshift");
 console.log(alturas);
 
+// INSERIR E REMOVER GERAL/QUALQUER LUGAR
+
+const letras = ["A", "B", "C", "D", "E", "F"];
+
+letras.splice(2, 3); //a partir da posicao n, delete y elementos
+console.log("TESTE SPLICE");
+console.log(letras);
+
 // CONCATENAR ARRAYS
 
 const meninas = ["Maria", "Ana"];
 const meninos = ["João", "Marcos"];
+
+const todos = meninas.concat(meninos);
+console.log("TESTE CONCAT");
+console.log(todos);
+
+// ACESSO E ATRIBUICAO
+
+const idades = [20, 30, 40, 50];
+
+idades[1] = 38;
+console.log("ACESSO E ATRIBUICAO");
+console.log(idades[1]);
+
+// PERCORRER ARRAY
+
+const frutas = ["bananas", "laranja", "uva"];
+
+console.log("TESTE PERCORRER ARRAY");
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
+
+console.log("PERCORRER ARRAY COM FOR EACH");
+frutas.forEach((elemento) => {
+  // lambda em JS
+  console.log(elemento);
+});
