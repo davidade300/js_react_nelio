ade6421c69e6cc009b5e251da5f2919ad4dc0c56
davidade300 <iaderinho@icloud.com>
2025-07-14 11:53:44 -0300

    estrutura de arquivos refatorada, codigos sobre json adicionados

---
+++ JavaScript/topicos_base_pt_2/json_/script.js
@@ -0,0 +1,53 @@
+const obj1 = {
+  //   "name": "computador", //pode ter as aspas ou não dentro do js
+  //   "price": 50.9,
+  "due-date": "2025-04-15", //nomes com caracteres speciais tem de ter apas
+};
+
+const obj2 = {
+  name: "computador",
+  price: 50.9,
+  "due-date": "2025-04-15",
+};
+
+const obj3 = {
+  id: 53,
+  date: "2021-10-20",
+  items: [
+    {
+      description: "Celular",
+      price: 1499.99,
+      quantity: 1,
+    },
+    {
+      description: "Mouse",
+      price: 100.0,
+      quantity: 2,
+    },
+  ],
+  client: {
+    name: "Maria Red",
+    email: "maria@gmail.com",
+    active: true,
+  },
+};
+
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
+
+console.log(obj3.id);
+console.log(obj3.client);
+console.log(obj3.client.name);
+
+// parse / stringfy
+
+const txt = `{"nome": "Computador", "price": 50.9, "due-date": "2025-04-15"}`;
+
+const obj = JSON.parse(txt); // transforma um txt em json
+
+console.log(txt.name);
+
+console.log(obj.nome);
+
+const text = JSON.stringify(obj);
