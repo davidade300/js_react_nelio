e0f4016a042cadf6ba5b59e8df351320d8e551e7
davidade300 <iaderinho@icloud.com>
2025-12-26 20:48:14 -0300

    catch request error

--- reactJs/dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
+++ reactJs/dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,22 +1,28 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import "./styles.css";
 import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
-import * as productService from "../../../services/product-service"
+import * as productService from "../../../services/product-service";
 
 export default function ProductDetails() {
     const params = useParams();
 
+    const navigate = useNavigate();
+
     const [product, setProduct] = useState<ProductDTO>();
 
     useEffect(() => {
-        productService.findById(Number(params.productId))
+        productService
+            .findById(Number(params.productId))
             .then((response) => {
                 console.log(response.data);
                 setProduct(response.data);
+            })
+            .catch(() => {
+                navigate("/");
             });
     }, []);
 
@@ -25,9 +31,7 @@ export default function ProductDetails() {
             <section id="product-details-section" className="dsc-container">
                 {product && <ProductDetailsCard product={product} />}
                 <div className="dsc-btn-page-container">
-                    {/* <Link to={}> */}
                     <ButtonPrimary text="Comprar" />
-                    {/* </Link> */}
                     <Link to={"/"}>
                         <ButtonInverse text="Inicio" />
                     </Link>
