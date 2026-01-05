492841643fdd20c4c59138983f5f03316f9d8cdf
davidade300 <iaderinho@icloud.com>
2026-01-05 15:27:21 -0300

    initial cart route component creation

--- reactJs/dscommerce/src/App.tsx
+++ reactJs/dscommerce/src/App.tsx
@@ -2,21 +2,21 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientHome from "./routes/ClientHome";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import Catalog from "./routes/ClientHome/Catalog";
+import Cart from "./routes/ClientHome/Cart";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ClientHome />}>
-          <Route index element={<Catalog />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route
-            path="product-details/:productId"
-            element={<ProductDetails />}
-          />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<ClientHome />}>
+                    <Route index element={<Catalog />} />
+                    <Route path="catalog" element={<Catalog />} />
+                    <Route path="product-details/:productId" element={<ProductDetails />}/>
+                    <Route path="cart" element={<Cart />} />
+
+                </Route>
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
