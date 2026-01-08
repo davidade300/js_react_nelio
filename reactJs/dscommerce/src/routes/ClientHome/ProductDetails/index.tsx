a7fb682b24869d5020d57bb46701f35a6ac80516
davidade300 <iaderinho@icloud.com>
2026-01-07 23:12:11 -0300

    fixed get for cart repo

--- reactJs/dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
+++ reactJs/dscommerce/src/routes/ClientHome/ProductDetails/index.tsx
@@ -6,6 +6,7 @@ import "./styles.css";
 import { useEffect, useState } from "react";
 import type { ProductDTO } from "../../../models/product";
 import * as productService from "../../../services/product-service";
+import * as cartService from "../../../services/cart-service";
 
 export default function ProductDetails() {
     const params = useParams();
@@ -26,12 +27,21 @@ export default function ProductDetails() {
             });
     }, []);
 
+    function handleBuyClick() {
+        if (product) {
+            cartService.addProduct(product);
+            navigate("/cart")
+        }
+    }
+
     return (
         <main>
             <section id="product-details-section" className="dsc-container">
                 {product && <ProductDetailsCard product={product} />}
                 <div className="dsc-btn-page-container">
-                    <ButtonPrimary text="Comprar" />
+                    <div onClick={handleBuyClick}>
+                        <ButtonPrimary text="Comprar" />
+                    </div>
                     <Link to={"/"}>
                         <ButtonInverse text="Inicio" />
                     </Link>
