4ee329744a9475bd94ee4e180290e6f5d4325b49
davidade300 <iaderinho@icloud.com>
2026-01-09 21:03:01 -0300

    incremento e decremento de cartItem

--- reactJs/dscommerce/src/routes/ClientHome/Cart/index.tsx
+++ reactJs/dscommerce/src/routes/ClientHome/Cart/index.tsx
@@ -13,6 +13,16 @@ export default function Cart() {
         setCart(cartService.getCart());
     }
 
+    function handleIncreaseItem(productId: number) {
+        cartService.increaseItem(productId);
+        setCart(cartService.getCart());
+    }
+
+    function handleDecreaseItem(productId: number) {
+        cartService.decreaseItem(productId);
+        setCart(cartService.getCart());
+    }
+
     return (
         <main>
             <section id="cart-container-section" className="dsc-container">
@@ -34,11 +44,13 @@ export default function Cart() {
                                     <div className="dsc-cart-item-description">
                                         <h3>{item.name}</h3>
                                         <div className="dsc-cart-item-quantity-container">
-                                            <div className="dsc-cart-item-quantity-btn">
+                                            <div onClick={() => handleDecreaseItem(item.productId)}
+                                                 className="dsc-cart-item-quantity-btn">
                                                 -
                                             </div>
                                             <p>{item.quantity}</p>
-                                            <div className="dsc-cart-item-quantity-btn">
+                                            <div onClick={() => handleIncreaseItem(item.productId)}
+                                                 className="dsc-cart-item-quantity-btn">
                                                 +
                                             </div>
                                         </div>
